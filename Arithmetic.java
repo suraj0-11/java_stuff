e775639e270202a9dea729bb943edab03e58d2c3
Suraj S <116282102+suraj0-11@users.noreply.github.com>
2022-11-07 23:20:11 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+/*If u are using online compiler i recommend u to change 
+Public class Main down below*/
 public class Arithmetic 
 {
 	public static void main(String[] args)
